2a621a3b6ce569d6882032c2411441e4ba84ed58
Evgeniya Katunina <evgeniya-katunina@yandex.ru>
2019-08-18 13:28:25 +0400

    Implement console parameters pass.

--- src/main/java/ru/frozen/gitextractor/App.java
+++ src/main/java/ru/frozen/gitextractor/App.java
@@ -1,5 +1,6 @@
 package ru.frozen.gitextractor;
 
+import java.io.Console;
 import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
@@ -7,24 +8,35 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * This is the test application for extracting a content from GitHub.
- *
  */
 public class App {
-	
-	private static final Logger log = LogManager.getLogger(App.class);
-	
-    public static void main(String[] args) {
-    	try {
-    		String url = "api.github.com";
-    		String user = "userName";
-    		String pass = "passName";
-    		String reponame = "repoName";
-    		String targetfolder = "myFolder";
-			new GitHubExtractor(url, user, pass).extract(reponame, new FileSystemApplier(targetfolder));
-		} catch (IOException e) {
-			log.error("Failed to extract from github.", e);
-		}
 
+    private static final Logger log = LogManager.getLogger(App.class);
+
+    private static final String GITHUB_API_URL = "api.github.com";
+    private static final String USAGE = "USAGE: .\\App repositoryName userName updateTimeINMinutes backupType " +
+            "pathToBackUp";
+    private static final String FILE_SYSTEM_BACKUP = "fileSystem";
+    private static final String PASSWORD_PROMPT = "Enter password:";
+    private static final String GITHUB_EXTRACT_FAIL_MSG = "Failed to extract from github.";
+
+    public static void main(String[] args) {
+        try {
+            String user = args[1];
+            String reponame = args[0];
+            String targetFolder = args[4];
+            if (args[3].equals(FILE_SYSTEM_BACKUP)) {
+                Console console = System.console();
+                char[] password = console.readPassword(PASSWORD_PROMPT);
+                new GitHubExtractor(GITHUB_API_URL, user, new String(password)).extract(reponame,
+                        new FileSystemApplier(targetFolder));
+            } else {
+                System.out.println(USAGE);
+            }
+        } catch (IOException e) {
+            log.error(GITHUB_EXTRACT_FAIL_MSG, e);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(USAGE);
+        }
     }
-    
 }
